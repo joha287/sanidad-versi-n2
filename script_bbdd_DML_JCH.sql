9dcffaf8cf68fb8d55739c17747a0415f6f44b73
joha287 <150701969+joha287@users.noreply.github.com>
2024-01-02 19:35:25 +0100

    Add files via upload

---
+++ script_bbdd_DML_JCH.sql
@@ -0,0 +1,21 @@
+
+
+
+INSERT INTO `sanidad`.`T_DOLENCIA` (`codigo`) VALUES ('01');
+INSERT INTO `sanidad`.`T_DOLENCIA` (`codigo`) VALUES ('02');
+INSERT INTO `sanidad`.`T_DOLENCIA` (`codigo`) VALUES ('03');
+
+INSERT INTO `sanidad`.`T_PLANTAS` (`id_planta`) VALUES ('1');
+INSERT INTO `sanidad`.`T_PLANTAS` (`id_planta`) VALUES ('2');
+INSERT INTO `sanidad`.`T_PLANTAS` (`id_planta`) VALUES ('3');
+
+INSERT INTO `sanidad`.`T_FUNCIONES` (`id_funcion`,`descripcion_de funcion`) VALUES ('10');
+INSERT INTO `sanidad`.`T_FUNCIONES` (`id_funcion`,`descripcion_de funcion`) VALUES ('20');
+INSERT INTO `sanidad`.`T_FUNCIONES` (`id_funcion`,`descripcion_de funcion`) VALUES ('30');
+
+
+INSERT INTO `sanidad`.`T_PERSONAL_PLANTAS_FUNCIONES` (`id_personal_medico`,`id_planta`,`id_funcion`) VALUES ('1000','1','01');
+INSERT INTO `sanidad`.`T_PERSONAL_PLANTAS_FUNCIONES` (`id_planta`) VALUES ('1001','2','02');
+
+
+
